13593f22d8374420d59213c313d179e70473de2f
MEDIUMTALE <105372200+MEDIUMTALE@users.noreply.github.com>
2024-11-07 22:02:44 +0500

    Upd 1.2
    Changes to Romchik

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def handle_buttons(message):
     if message.text == "Информация о VPN 📜":
         bot.send_message(message.chat.id, text1)
     elif message.text == "Тарифы 📚":
-        tarif_photo = open("./tarif.png", "rb")
+        tarif_photo = open("ViewModels/resourse/img/tarif.png", "rb")
         bot.send_photo(message.chat.id, tarif_photo)
     elif message.text == "Получить ключ 🔑":
         tarif_menu(message)
@@ -62,8 +62,7 @@ def handle_buttons(message):
     elif message.text == "Для Android 🤖":
         bot.send_message(message.chat.id, "в разработке...")
     elif message.text == "Для Iphone 🍎":
-        bot.send_message(message.chat.id, "в разработке...")
-
+        guide_iphone(message)
     else:
         bot.send_message(message.chat.id, "Я не понимаю, выберите нужный пункт снизу.")
 
@@ -95,4 +94,39 @@ def guide_menu(message):
 
     bot.send_message(message.chat.id, "Выверите инструкцию подходящую под ваше устройство, или вернитесь в меню.", reply_markup=markup)
 
+
+
+def guide_iphone(message):
+    # подгрузка фотографий
+    in_appstore_photo = open("ViewModels/resourse/img/guides/iphone/in_appstore.jpg", "rb")
+    add_profile_photo = open("ViewModels/resourse/img/guides/iphone/add_profile.jpg", "rb")
+    import_at_bufer_photo = open("ViewModels/resourse/img/guides/iphone/import_at_bufer.jpg", "rb")
+    profile_choice_photo = open("ViewModels/resourse/img/guides/iphone/profile_choice.jpg", "rb")
+    activate_photo = open("ViewModels/resourse/img/guides/iphone/activate.jpg", "rb")
+
+    # шаг 1
+    bot.send_message(message.chat.id,
+                     "Шаг 1:\n    Для запуска нашего VPN на Iphone, необходимо приложение клиент, в нашем случаее рекомендую 'Streisand':")
+    bot.send_photo(message.chat.id, in_appstore_photo)
+
+    # шаг 2
+    bot.send_message(message.chat.id,
+                     "Шаг 2:\n    Предварительно скопировав выданный ключ, зайдя в приложение нажмите на плюсик в верхнем правом углу экрана:")
+    bot.send_photo(message.chat.id, add_profile_photo)
+
+    # шаг 3
+    bot.send_message(message.chat.id, "Шаг 3:\n    Нажмите на кнопку 'Добавить из буфера':")
+    bot.send_photo(message.chat.id, import_at_bufer_photo)
+
+    # шаг 4
+    bot.send_message(message.chat.id, "Шаг 4:\n    Выберите добавленный профиль нажав на него:")
+    bot.send_photo(message.chat.id, profile_choice_photo)
+
+    # шаг 5
+    bot.send_message(message.chat.id, "Шаг 5:\n    Включите VPN:")
+    bot.send_photo(message.chat.id, activate_photo)
+
 bot.polling(none_stop=True)
+
+
+
